5a2772c11539e91246fa308e242fadc9dc7dc63f
MikeBane57 <MichaelRyanBane@gmail.com>
2026-04-06 09:15:31 -0500

    Add update and download URLs to user script
    Added update and download URLs for the script.

--- ADSB-flightAware from flight pucks.user.js
+++ ADSB-flightAware from flight pucks.user.js
@@ -6,6 +6,8 @@
 // @match        https://opssuitemain.swacorp.com/*
 // @grant        GM_openInTab
 // @connect      adsbexchange.com
+// @updateURL    https://github.com/MikeBane57/Wolf2.0/blob/main/ADSB-flightAware%20from%20flight%20pucks.user.js
+// @downloadURL  https://github.com/MikeBane57/Wolf2.0/blob/main/ADSB-flightAware%20from%20flight%20pucks.user.js
 // ==/UserScript==
 
 
